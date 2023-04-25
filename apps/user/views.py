212c54fe096621e950e680e8847ea7882d5524b3
kermitlafrog61 <islamchikalybaev@gmail.com>
2023-04-25 20:29:29 +0600

    resend activation, reset password & reset username emails

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,12 +1,15 @@
-from djoser.views import UserViewSet
-from djoser.compat import get_user_email
-from djoser import signals, utils
+from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
-
-
-from .tasks import send_activation_email
-from rest_framework.response import Response
+from djoser import signals, utils
+from djoser.conf import settings as djoser_settings
+from djoser.views import UserViewSet
 from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .tasks import send_registration, send_reset_password, send_reset_username
+
+User = get_user_model()
 
 
 class CustomUserViewSet(UserViewSet):
@@ -20,11 +23,64 @@ class CustomUserViewSet(UserViewSet):
             sender=self.__class__, user=user, request=self.request
         )
 
-        context = {}
+        context = {
+            'uid': utils.encode_uid(user.pk),
+            'token': default_token_generator.make_token(user),
+            'domain': self.request.build_absolute_uri('/')
+        }
 
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-
-        send_activation_email.delay(user.pk, context)
+        send_registration.delay(user.pk, context)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(["post"], detail=False)
+    def resend_activation(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.get_user(is_active=False)
+
+        if not djoser_settings.SEND_ACTIVATION_EMAIL or not user:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        context = {
+            'uid': utils.encode_uid(user.pk),
+            'token': default_token_generator.make_token(user),
+            'domain': self.request.build_absolute_uri('/')
+        }
+
+        send_registration.delay(user.pk, context)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(["post"], detail=False)
+    def reset_password(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.get_user()
+
+        if user:
+            context = {
+                'uid': utils.encode_uid(user.pk),
+                'token': default_token_generator.make_token(user),
+                'domain': self.request.build_absolute_uri('/')
+            }
+
+            send_reset_password.delay(user.pk, context)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(["post"], detail=False, url_path=f"reset_{User.USERNAME_FIELD}")
+    def reset_username(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.get_user()
+
+        if user:
+            context = {
+                'uid': utils.encode_uid(user.pk),
+                'token': default_token_generator.make_token(user),
+                'domain': self.request.build_absolute_uri('/')
+            }
+            send_reset_username.delay(user.pk, context)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
