fdbf36accc32db1b363e4e801c0c01a6532c803d
kermitlafrog61 <islamchikalybaev@gmail.com>
2023-04-27 12:23:29 +0600

    user_profile

--- apps/user/permissions.py
+++ apps/user/permissions.py
@@ -3,10 +3,6 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, user):
-        print('test')
-        print(user)
-        print(request.method)
-        print(request.user)
         return bool(
             request.method in SAFE_METHODS or
             (request.user and request.user.is_authenticated and
