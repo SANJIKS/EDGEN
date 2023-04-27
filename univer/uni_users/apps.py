86de8c271196472ccfb9b1cab8b5c43d0acfd4b7
kermitlafrog61 <islamchikalybaev@gmail.com>
2023-04-27 12:28:28 +0600

    deleted univer for main

--- univer/uni_users/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UniUsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'univer.uni_users'
--- univer/uni_users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import OrganizationRegistration
-
-class OrganizationRegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrganizationRegistration
-        exclude = ('approved',)
